9209521d602dfd0fec84e2b1630d698b6bf09c71
tacman <info@survos-ci.com>
2024-08-25 12:57:46 +0000

    add commands, basic documentation

--- src/SurvosBunnyBundle.php
+++ src/SurvosBunnyBundle.php
@@ -4,10 +4,14 @@
 
 namespace Survos\BunnyBundle;
 
+use Survos\BunnyBundle\Command\BunnyListCommand;
+use Survos\BunnyBundle\Controller\BunnyController;
 use Survos\BunnyBundle\Service\BunnyService;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class SurvosBunnyBundle extends AbstractBundle
@@ -18,30 +22,32 @@ class SurvosBunnyBundle extends AbstractBundle
         $container->services()->alias(BunnyService::class, $serviceId);
         $builder->autowire($serviceId, BunnyService::class)
             ->setArgument('$apiKey', $config['api_key'])
+            ->setArgument('$readonlyPassword', $config['readonly_password'])
+            ->setArgument('$password', $config['password'])
             ->setArgument('$storageZone', $config['storage_zone'])
             ->setAutoconfigured(true)
             ->setAutowired(true)
             ->setPublic(true);
 
-        $builder->autowire(BunnCon::class)
+        $builder->autowire(BunnyController::class)
             ->addTag('container.service_subscriber')
             ->addTag('controller.service_arguments')
-            ->setArgument('$bus', new Reference('debug.traced.messenger.bus.default', ContainerInterface::NULL_ON_INVALID_REFERENCE))
-            ->setArgument('$chartBuilder', new Reference('chartjs.builder', ContainerInterface::NULL_ON_INVALID_REFERENCE))
         ;
 
-        // $builder->setParameter('survos_workflow.direction', $config['direction']);
+        foreach ([BunnyListCommand::class] as $commandName) {
+            $builder->autowire($commandName)
+                ->setAutoconfigured(true)
+                ->addTag('console.command')
+            ;
+        }
 
-        // twig classes
 
+        // twig classes, for bunny_url
         /*
         $definition = $builder
         ->autowire('survos.barcode_twig', BarcodeTwigExtension::class)
         ->addTag('twig.extension');
 
-        $definition->setArgument('$widthFactor', $config['widthFactor']);
-        $definition->setArgument('$height', $config['height']);
-        $definition->setArgument('$foregroundColor', $config['foregroundColor']);
         */
     }
 
@@ -50,6 +56,8 @@ class SurvosBunnyBundle extends AbstractBundle
         $definition->rootNode()
             ->children()
                 ->scalarNode('api_key')->defaultValue(null)->end()
+                ->scalarNode('readonly_password')->defaultValue(null)->end()
+                ->scalarNode('password')->defaultValue(null)->end()
                 ->scalarNode('storage_zone')->defaultValue(null)->end()
 //            ->integerNode('cache')->defaultValue('1h')->end()
             ->end();
